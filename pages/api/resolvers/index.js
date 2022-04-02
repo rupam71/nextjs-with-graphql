0f1ccc6c3377b7cdf428792df9c8450259196909
Rupam Sahriar <rupamsahriar@gmail.com>
2022-04-02 17:55:05 +0600

    add mutation and some tips

--- pages/api/resolvers/index.js
+++ pages/api/resolvers/index.js
@@ -1,32 +1,42 @@
 import axios from "axios";
+import userList from "./../../../userList.json"
 
 const resolvers = {
   Query: {
     getUsers: async () => {
       try {
-        const users = await axios.get("https://api.github.com/users");
-        return users.data.map(({ id, login, avatar_url }) => ({
-          id,
-          login,
-          avatar_url
-        }));
+        // const users = await axios.get("https://api.github.com/users");
+        // return users.data.map((user) => ({
+        //   user
+        // }));
+        return userList;
       } catch (error) {
         throw error;
       }
     },
     getUser: async (_, args) => {
       try {
-        const user = await axios.get(
-          `https://api.github.com/users/${args.name}`
-        );
-        return {
-          id: user.data.id,
-          login: user.data.login,
-          avatar_url: user.data.avatar_url
-        };
+        // const user = await axios.get(
+        //   `https://api.github.com/users/${args.name}`
+        // );
+        // return {
+        //   id: user.data.id,
+        //   login: user.data.login,
+        //   avatar_url: user.data.avatar_url
+        // };
+        const user = userList.filter((ele)=> ele.name===args.name)
+        console.log('user',user[0])
+        return user[0]
       } catch (error) {
         throw error;
       }
+    }
+  },
+  Mutation: {
+    createUser(parants,args) {
+      const newUser = args
+      userList.push(newUser)
+      return newUser;
     }
   }
 };
--- pages/api/schemas/index.js
+++ pages/api/schemas/index.js
@@ -2,14 +2,26 @@ import { gql } from '@apollo/client';
 
 const  typeDefs  =  gql`
     type  User {
-        id: ID
+        id: ID!
+        name: String!
+        age: Int!
+        married: Boolean!
         login: String
         avatar_url: String
     }
 
+    # Queries
     type  Query {
         getUsers: [User]
         getUser(name: String!): User!
-    }`
+    }
+    
+    # Mutation
+    type Mutation{
+        createUser(name: String!, age: Int, married: Boolean) : User!
+       # createNewUser(User) : User!
+       # createNewUserMust(User!) : User!
+    }
+    `
 
 export default typeDefs
